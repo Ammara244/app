66689191a059c768ed90b3a19f4ac50d9c41f456
Ammara244 <ammarahajat24@gmail.com>
2025-08-30 00:49:40 +0100

    Update finances.py

--- pages/finances.py
+++ pages/finances.py
@@ -1,9 +1,33 @@
-import pandas as pd
+st.title('bank balance simulator')
 
-st.title('MONEY')
+# creating account
+if "balance" not in st.session_state:
+    st.session_state.balance = 0
 
+# placeholder on page where balance will always be displayed
+bajance_display = st.empty()
+balance_display.text('Your current bank balance is: £' ,st.session_state.balance)
 
+# add initial balance
+initial_balance = st.number_input(
+    "Enter Current Balance (£):", 
+    min_value=0,         # makes sure they dont put negative number
+    value=st.session_state.balance, # aint got a flipping clue in the world cus chatgpf is a stupid prick
+    # something about setting a value so the input box always shows your balance
+    step=1 # i think this lets the pick a number by pressing the up/down
+)
+balance_display.text('Your current bank balance is: £' ,st.session_state.balance)
+
+st.write('Your current bank balance is ', st.session_state.balance)
 
 if st.button('Add Money'):
-  amount = st.text_input('Enter Amount')
+  amount = st.number_input('Enter Amount')
+  st.session_state.balance += amount
+  st.success('You added ', amount, ' to your account!')
+  balance_display.text('Your current bank balance is: £', st.session_state.balance)
   
+if st.button('Withdraw Money'):
+  amount = st.number_input('Enter Amount')
+  st.session_state.balance -= amount
+  st.success('You withdrew ', amount, ' from your account!')
+  balance_display.text('Your current bank balance is: £', st.session_state.balance)
