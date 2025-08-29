f4527e99b2d027a0ebbe0287d7bd864994d69e90
Ammara244 <ammarahajat24@gmail.com>
2025-08-29 23:46:00 +0100

    Update 2_Todo_List.py

--- pages/2_Todo_List.py
+++ pages/2_Todo_List.py
@@ -2,16 +2,20 @@ import streamlit as st
 
 st.title("📝 To-Do List")
 
+# makes list
 if "tasks" not in st.session_state:
     st.session_state.tasks = []
 
 new_task = st.text_input("Add a new task")
+# when button is clicked and text isnt empty
 if st.button("➕ Add Task") and new_task:
     st.session_state.tasks.append(new_task)
     st.success(f"Added: {new_task}")
 
+# if there are tasks, show them
 if st.session_state.tasks:
     st.subheader("Your Tasks")
+    # numbers each task, starting at 1
     for i, task in enumerate(st.session_state.tasks, start=1):
         st.write(f"{i}. {task}")
 else:
