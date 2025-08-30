b32fd8b4a52f619c7bf4a1213f53f4bb207e0930
Ammara244 <ammarahajat24@gmail.com>
2025-08-30 01:02:25 +0100

    Update finances.py

--- pages/finances.py
+++ pages/finances.py
@@ -5,8 +5,8 @@ if "balance" not in st.session_state:
     st.session_state.balance = 0
 
 # placeholder on page where balance will always be displayed
-bajance_display = st.empty()
-balance_display.text('Your current bank balance is: £' ,st.session_state.balance)
+balance_display = st.empty()
+balance_display.text(f'Your current bank balance is: £{st.session_state.balance}')
 
 # add initial balance
 initial_balance = st.number_input(
@@ -16,18 +16,18 @@ initial_balance = st.number_input(
     # something about setting a value so the input box always shows your balance
     step=1 # i think this lets the pick a number by pressing the up/down
 )
-balance_display.text('Your current bank balance is: £' ,st.session_state.balance)
+balance_display.text(f'Your current bank balance is: £{st.session_state.balance}')
 
 st.write('Your current bank balance is ', st.session_state.balance)
 
 if st.button('Add Money'):
-  amount = st.number_input('Enter Amount')
-  st.session_state.balance += amount
-  st.success('You added ', amount, ' to your account!')
-  balance_display.text('Your current bank balance is: £', st.session_state.balance)
+    amount = st.number_input('Enter Amount')
+    st.session_state.balance += amount
+    st.success(f'You added £{amount} to your account!')
+    balance_display.text(f'Your current bank balance is: £{st.session_state.balance}')
   
 if st.button('Withdraw Money'):
-  amount = st.number_input('Enter Amount')
-  st.session_state.balance -= amount
-  st.success('You withdrew ', amount, ' from your account!')
-  balance_display.text('Your current bank balance is: £', st.session_state.balance)
+    amount = st.number_input('Enter Amount')
+    st.session_state.balance -= amount
+    st.success(f'You withdrew £{amount} from your account!')
+    balance_display.text(f'Your current bank balance is: £{st.session_state.balance}')
