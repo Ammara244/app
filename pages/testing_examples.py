d2912bee0ca1cefe72733e687ef2bed738cf6665
Ammara244 <ammarahajat24@gmail.com>
2025-09-25 14:02:45 +0100

    Update testing_examples.py

--- pages/testing_examples.py
+++ pages/testing_examples.py
@@ -1,22 +1,22 @@
 import streamlit as py
 
-st.subheader('slider widget')
+st.header('slider widget')
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
 
-st.subheader('creating a key:')
+st.header('creating a key:')
 import streamlit as st
 st.text_input("Your name", key="name")
 
 # You can access the value at any point with:
 st.session_state.name
 
-st.subheader('checkbox:')
+st.header('checkbox:')
 if st.checkbox('Show '):
   st.write('boo')
 
 
-st.subheader('selectbox:')
+st.header('selectbox:')
 import pandas as pd
 
 df = pd.DataFrame({
